565fd67568eb2ca41fc35745c067dc0d0a8ae948
tensoradam <adamfarooq07@gmail.com>
2024-07-03 14:24:49 +0500

    Update ./Esperf.h

---
+++ Esperf.h
@@ -0,0 +1,27 @@
+//
+// Created by Kosho Owa on 2016/08/19.
+//
+
+#ifndef ESPERF_ESPERF_H
+#define ESPERF_ESPERF_H
+
+#include <thread>
+
+#include "Options.h"
+#include "Stats.h"
+#include "Worker.h"
+#include "Timer.h"
+
+using namespace std;
+
+class Esperf {
+public:
+    Esperf(Options *options);
+    void Run();
+private:
+    Options *options_;
+    mutex mtx_for_cout_;
+};
+
+
+#endif //ESPERF_ESPERF_H
